23bc6271e52031cd05891958dcd8060fcf19768f
aligos <artasikindonesia@gmail.com>
2016-03-21 23:36:20 +0700

    fbconnect to user are exist

---
+++ fb-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '188312054889738',
+    secret: 'd2d9344a0e941732fd4b71d8e673e167'
+});
