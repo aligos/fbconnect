13178a6a0131a70e430cc9ebb531297296f21c27
aligos <artasikindonesia@gmail.com>
2016-03-23 17:02:51 +0700

    update fbconnect

--- fbconnect.js
+++ fbconnect.js
@@ -1,11 +1,18 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  Package.blaze.Blaze.Template.registerHelper('noFB', function () {
+      return !Meteor.user().services.facebook;
+  });
 
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    userFb: function () {
+      return Meteor.users().services.facebook;
     }
+
   });
 
   Template.hello.events({
